e56e887f783301b1c033490bdad7bc1dbfd59e06
Your Name <you@example.com>
2025-12-16 09:02:30 +0100

    docker verfeinert

---
+++ docker/docker_install.sh
@@ -0,0 +1,25 @@
+sudo -i
+apt update
+apt install apt-transport-https curl -y 
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg  \ 
+     | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) \ 
+      signed-by=/etc/apt/keyrings/docker.gpg] \ 
+      https://download.docker.com/linux/ubuntu \ 
+      $(. /etc/os-release && echo "$VERSION_CODENAME") \ 
+      stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+
+apt install docker-ce docker-ce-cli \ 
+    containerd.io docker-buildx-plugin \ 
+    docker-compose-plugin -y
+
+
+compose_url="https://github.com/docker/compose/releases/download"
+curl -L  $compose_url/1.28.5/docker-compose-`uname \ 
+     -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
