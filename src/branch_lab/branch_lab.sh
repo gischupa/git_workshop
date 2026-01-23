d147f0f269b69edb8947fb45ebdab876133e4065
Wolfgang <hoeferwolf@t-online.de>
2026-01-23 14:15:27 +0100

    merge update

--- src/branch_lab/branch_lab.sh
+++ src/branch_lab/branch_lab.sh
@@ -1,1 +1,22 @@
+git init branch_lab
+cd branch_lab 
+git branch -m main
+
+echo "Zeile 1" > datei.txt 
+git add .
+git commit -m "Datei 1 erstellt"
+
+git log
+
+########################
+
+echo "Zeile 2" >> datei.txt 
+git add . && git commit -m "Zeile 2"
+
+echo "Zeile 3" >> datei.txt 
+git add . && git commit -m "Zeile 3"
+
+git log --oneline 
+
+########################
 
