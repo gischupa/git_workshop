18b18b49d3fa83ea376d0743d3a246cd138d7c43
wolfgang <hoeferwolf@t-online.de>
2026-02-02 14:57:49 +0100

    konsolidierung

--- src/branch_lab/branch_lab.sh
+++ src/branch_lab/branch_lab.sh
@@ -1,3 +1,6 @@
+cd /tmp
+rm -rf lab
+
 git init branch_lab
 cd branch_lab 
 git branch -m main
@@ -6,7 +9,7 @@ echo "Zeile 1" > datei.txt
 git add .
 git commit -m "Datei 1 erstellt"
 
-git log
+git log --oneline
 
 ########################
 
@@ -16,7 +19,12 @@ git add . && git commit -m "Zeile 2"
 echo "Zeile 3" >> datei.txt 
 git add . && git commit -m "Zeile 3"
 
-git log --oneline 
+git log --oneline  # HEAD->main
 
 ########################
 
+git switch -c arbeit
+git log --oneline  # HEAD->arbeit, main
+
+git switch main
+git log --oneline  # HEAD->main, arbeit
